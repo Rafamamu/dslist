29ab734c36809aea390215e24a602f5346123efb
Rafael <rafaroga46@gmail.com>
2025-01-22 20:11:24 -0300

    Project created

---
+++ src/main/java/com/rafaroga46/dslist/DslistApplication.java
@@ -0,0 +1,14 @@
+package com.rafaroga46.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistApplication.class, args);
+		System.out.println("Executado com Sucesso...");
+	}
+
+}
